6a46051305caf87c378d8a729a2650c9943c23b3
0xpanicError <theyashsaraswat@gmail.com>
2023-04-23 06:36:05 +0530

    update multisig contract

--- contracts/my-sudt/src/try2.rs
+++ contracts/my-sudt/src/try2.rs
@@ -1,101 +1,75 @@
-#![no_std]
-#![no_main]
-#![feature(lang_items)]
-#![feature(alloc_error_handler)]
-#![feature(panic_info_message)]
+use secp256k1::hashes::sha256;
+use secp256k1::{Message, Secp256k1};
+// use std::any::type_name;
 
-use alloc::vec::Vec;
-use core::result::Result;
+// fn type_of<T>(_: T) -> &'static str {
+//     type_name::<T>()
+// }
 
-use ckb_std::{
-    ckb_constants::Source,
-    ckb_types::{bytes::Bytes, prelude::*},
-    debug, default_alloc,
-    error::SysError,
-    high_level::{load_script, load_witness_args},
-    secp256k1,
-};
+#[derive(Debug)]
+enum Error {
+    VectorLengthNotSame,
+}
 
-use crate::error::Error;
+fn verify_signature(
+    message: &Message,
+    signature: &secp256k1::ecdsa::Signature,
+    public_key: &secp256k1::PublicKey,
+) -> bool {
+    let secp = Secp256k1::new();
+    secp.verify_ecdsa(message, signature, public_key).is_ok()
+}
 
-mod error;
-
-// Constants
-const THRESHOLD_BYTES: usize = 1;
-const PUBKEY_HASH_BYTES: usize = 20;
-
-entry!(entry);
-default_alloc!();
-
-fn entry() -> i8 {
-    match main() {
-        Ok(_) => 0,
-        Err(err) => err as i8,
+fn validate_multisig(
+    threshold: u32,
+    messages: &[Message],
+    signatures: &[secp256k1::ecdsa::Signature],
+    public_keys: &[secp256k1::PublicKey],
+) -> Result<bool, Error> {
+    if messages.len() != signatures.len() {
+        return Err(Error::VectorLengthNotSame);
+    }
+    if messages.len() != public_keys.len() {
+        return Err(Error::VectorLengthNotSame);
+    } else {
+        let mut valid_sig = 0;
+        for i in 0..messages.len() {
+            let pass = verify_signature(&messages[i], &signatures[i], &public_keys[i]);
+            if pass {
+                valid_sig += 1;
+            }
+        }
+        return Ok(valid_sig >= threshold);
     }
 }
 
-fn main() -> Result<(), Error> {
-    let script = load_script()?;
-    let args: Bytes = script.args().unpack();
-    let threshold = args[0];
-    let num_pubkey_hashes = (args.len() - THRESHOLD_BYTES) / PUBKEY_HASH_BYTES;
+fn main() {
+    let threshold = 2;
 
-    if threshold as usize > num_pubkey_hashes {
-        return Err(Error::Threshold);
+    let secp = Secp256k1::new();
+
+    let (secret_key1, public_key1) = secp.generate_keypair(&mut rand::thread_rng());
+    let (secret_key2, public_key2) = secp.generate_keypair(&mut rand::thread_rng());
+    let message = Message::from_hashed_data::<sha256::Hash>("Hello World!".as_bytes());
+    let sig1 = secp.sign_ecdsa(&message, &secret_key1);
+    let sig2 = secp.sign_ecdsa(&message, &secret_key2);
+
+    let messages = vec![message, message];
+    let signatures = vec![sig1, sig2];
+    let public_keys = vec![public_key1, public_key2];
+
+    let result: Result<bool, Error> =
+        validate_multisig(threshold, &messages, &signatures, &public_keys);
+    match result {
+        Ok(value) => println!("The result is: {}", value),
+        Err(e) => println!("An error occurred: {:?}", e),
     }
 
-    let mut pubkey_hashes = Vec::with_capacity(num_pubkey_hashes);
-    for i in 0..num_pubkey_hashes {
-        let start = THRESHOLD_BYTES + i * PUBKEY_HASH_BYTES;
-        let end = start + PUBKEY_HASH_BYTES;
-        pubkey_hashes.push(args.slice(start, end));
-    }
-
-    let mut sigs_count = 0;
-
-    for i in 0.. {
-        let witness_args = match load_witness_args(i, Source::Input) {
-            Ok(witness_args) => witness_args,
-            Err(SysError::ItemMissing) => break,
-            Err(err) => return Err(err.into()),
-        };
-
-        let lock = witness_args.lock().to_opt().ok_or(Error::WitnessLock)?;
-
-        if lock.len() != 65 {
-            return Err(Error::SignatureSize);
-        }
-
-        let mut signature_data = [0u8; 64];
-        signature_data.copy_from_slice(&lock[..64]);
-        let signature = secp256k1::Signature::parse(&signature_data);
-
-        let recid = secp256k1::RecoveryId::parse(lock[64]).map_err(|_| Error::InvalidSignature)?;
-
-        let mut hasher = blake2b_simd::Params::new().hash_length(32).to_state();
-        hasher.update(&witness_args.as_bytes());
-        let message = hasher.finalize();
-
-        let pubkey = secp256k1::recover(
-            &secp256k1::Message::parse(message.as_array_ref().unwrap()),
-            &signature,
-            &recid,
-        )
-        .map_err(|_| Error::InvalidSignature)?;
-
-        let pubkey_hash = {
-            let mut hasher = blake2b_simd::Params::new().hash_length(20).to_state();
-            hasher.update(&pubkey.serialize_compressed());
-            hasher.finalize()
-        };
-
-        if pubkey_hashes.iter().any(|hash| &pubkey_hash == hash) {
-            sigs_count += 1;
-            if sigs_count >= threshold as usize {
-                return Ok(());
-            }
-        }
-    }
-
-    Err(Error::ThresholdNotMet)
+    // let sig = secp.sign_ecdsa(&message, &secret_key);
+    // let pass = verify_signature(&message, &sig, &public_key);
+    // println!("signature: {:?}", sig);
+    // println!("public key: {:?}", public_key);
+    // println!("pass: {:?}", pass);
+    // println!("message type: {}", type_of(&message));
+    // println!("signature type: {}", type_of(&sig));
 }
